b6d50eb428874bf1fa69afb39e6c63e2e793a5df
cschweda <cschweda@gmail.com>
2017-01-31 16:32:41 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ICJIA Simple Webpack Example
+
+Basic walkthrough of how Webpack bundles javascript and generates a build directory. This is loosely based on the Webpack 2 *Getting Started* example: (https://webpack.js.org/get-started/)[https://webpack.js.org/get-started/]
+
+## Install dependencies
+
+```
+npm install
+```
+
+## Run development server
+
+```
+npm run dev
+```
+
+## Serve the built files from the ```./dist``` directory
+
+```
+npm run serve
+```
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './index.html'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
